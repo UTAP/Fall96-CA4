f9cd7df2ce4cbd000a4db9fcfab3e7c96c037235
h@di <hadi@hadisafari.ir>
2018-01-30 17:34:39 +0330

    make minor changes in script

--- run.sh
+++ run.sh
@@ -10,10 +10,13 @@ NC='\033[0m' # No Color
 
 MAINS=(main.cpp main-with-extra-players.cpp main-with-penalty.cpp main-with-inaquate-teams.cpp main-bigger-testcase.cpp)
 counter=1
+if [[ -d $OUTPUT_DIR ]]; then
+	rm -rf $OUTPUT_DIR
+fi
+mkdir $OUTPUT_DIR
 for file in ${MAINS[*]}; do
-	mkdir $OUTPUT_DIR
 	yes | cp -i $file "main.cpp"
-	make clean && make && ./a.out > $OUTPUT_DIR/out_$counter
+	make clean && make && ./${1} > $OUTPUT_DIR/out_$counter
 	echo Testcase $counter is running:
 	if diff $OUTPUT_DIR/out_$counter $TEST_CASE_DIR/out_$counter > /dev/null; then
 		echo -e "${GREEN}Accepted${NC}"
@@ -22,6 +25,8 @@ for file in ${MAINS[*]}; do
 		echo -e "${RED}Wrong Answer${NC}"
 		((wrong_test_cases++))
 	fi
-
 	((counter++))
 done
+
+echo "=========================="
+echo -e "${GREEN}passed${NC}: $accepted_test_cases\t${RED}failed${NC}: $wrong_test_cases"
