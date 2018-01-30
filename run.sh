2cacdb3dd04c6a505ba32b617302b4f15edebd3a
h@di <hadi@hadisafari.ir>
2018-01-30 17:44:03 +0330

    make minor changes in script

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ TEST_CASE_DIR=testcase
 # defining colors
 RED="\033[0;31m"
 GREEN="\033[0;32m"
+CYAN="\033[0;36m"
 NC='\033[0m' # No Color
 
 MAINS=(main.cpp main-with-extra-players.cpp main-with-penalty.cpp main-with-inaquate-teams.cpp main-bigger-testcase.cpp)
@@ -14,10 +15,12 @@ if [[ -d $OUTPUT_DIR ]]; then
 	rm -rf $OUTPUT_DIR
 fi
 mkdir $OUTPUT_DIR
+accepted_test_cases=0
+wrong_test_cases=0
 for file in ${MAINS[*]}; do
-	yes | cp -i $file "main.cpp"
-	make clean && make && ./${1} > $OUTPUT_DIR/out_$counter
-	echo Testcase $counter is running:
+	echo -e "${CYAN}Testcase $counter ($file)${NC}"
+	cp -f $file "main.cpp"
+	make && ./${1} > $OUTPUT_DIR/out_$counter
 	if diff $OUTPUT_DIR/out_$counter $TEST_CASE_DIR/out_$counter > /dev/null; then
 		echo -e "${GREEN}Accepted${NC}"
 		((accepted_test_cases++))
@@ -26,7 +29,8 @@ for file in ${MAINS[*]}; do
 		((wrong_test_cases++))
 	fi
 	((counter++))
+	echo ""
 done
 
-echo "=========================="
+echo -e "${CYAN}==========================${NC}"
 echo -e "${GREEN}passed${NC}: $accepted_test_cases\t${RED}failed${NC}: $wrong_test_cases"
